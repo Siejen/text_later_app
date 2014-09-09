bb7d4c6408bd17512527a89746828b7579008ec8
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-09-08 22:11:52 -0700

    updated rspec files for models

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Contact, :type => :model do
+ 
+  subject { FactoryGirl.create(:contact) }
 
-	it "has necessary attributes" do
-		c = Contact.new(first_name: "Jerry", last_name: "Wong", phone_num: "23478900983")
-		expect(c.first_name).to eq("Jerry")
-		expect(c.last_name).to eq("Wong")
-		expect(c.phone_num).to eq("23478900983")				
-	end
+  it "should create a new contact" do
+    expect(subject).to be_valid
+  end
+
+
+	# it "has necessary attributes" do
+	# 	c = Contact.new(first_name: "Jerry", last_name: "Wong", phone_num: "23478900983")
+	# 	expect(c.first_name).to eq("Jerry")
+	# 	expect(c.last_name).to eq("Wong")
+	# 	expect(c.phone_num).to eq("23478900983")				
+	# end
 
 end
